11b91e90b071645ae9b33ab046926a74ec2a6687
Jordan George <jordanngeorge@gmail.com>
2018-10-13 17:54:27 -0400

    create map and center on UK

--- ios-midterm-2/ViewController.swift
+++ ios-midterm-2/ViewController.swift
@@ -7,14 +7,44 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupViews()
+        
+        // set initial location to UK
+        let initialLocation = CLLocation(latitude: 38.030672, longitude: -84.504160)
+        centerMapOnLocation(location: initialLocation)
     }
-
-
+    
+    let regionRadius: CLLocationDistance = 1000
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegion(center: location.coordinate,
+                                                  latitudinalMeters: regionRadius,
+                                                  longitudinalMeters: regionRadius)
+        map.setRegion(coordinateRegion, animated: true)
+    }
+    
+    // MARK: - views
+    
+    func setupViews() {
+        view.addSubview(map)
+        setupMap()
+    }
+    
+    lazy var map: MKMapView = {
+        let map = MKMapView()
+        map.translatesAutoresizingMaskIntoConstraints = false
+        
+        return map
+    }()
+    
+    func setupMap() {
+        map.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        map.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+    }
 }
 
